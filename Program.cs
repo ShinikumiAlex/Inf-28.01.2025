82d6191b39b909d85c5f68eb95a9d61b14b2b68e
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2025-01-28 09:01:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,14 +5,18 @@ class Program
     static void Main()
     {
         int a1 = 1; 
-        int d = 4;
-        int n = 5;
+        int d = 4;  
+        int n = 5;  
+        int sum = 0; 
 
         Console.WriteLine("Первые пять членов арифметической прогрессии:");
         for (int i = 0; i < n; i++)
         {
-            int currentTerm = a1 + i * d;
-            Console.Write(currentTerm + " ");
+            int currentTerm = a1 + i * d; 
+            Console.Write(currentTerm + " "); 
+            sum += currentTerm; 
         }
+
+        Console.WriteLine($"\nСумма: {sum}");
     }
 }
